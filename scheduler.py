dd270ee721a9796594866afe1895ba8c60f13060
debobrototalukder <debobrototalukder@gmail.com>
2019-08-04 14:18:50 +0400

    Sending email using Grid

--- scheduler.py
+++ scheduler.py
@@ -4,17 +4,41 @@ import psycopg2
 import schedule
 import time
 import smtplib
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+from sendgrid import SendGridAPIClient
 
+import json
 
 date_format = "%Y-%m-%d"
 
 connection = False
 cursor = None
 
+sg = sendgrid.SendGridAPIClient('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')
 
-def sendEmail(user_email, toUser_birthday):
+
+def sendEmailGrid(toUser_birthdayGrid, user_emailGrid):
+    message = Mail(
+        from_email='adminteam@chartercross.com',
+        to_emails= user_emailGrid,
+        subject="It's your birthday!",
+        html_content="<strong>Happy birthday " + toUser_birthdayGrid + "</strong>" + "<br> <br>Best Regards<br>Admin Team")
+
+    try:
+        msg = sg.send(message)
+
+    except Exception as e:
+        print(e)
+
+    print("Email to birthday user sent!\n")
+
+
+def sendEmail(toUser_birthday, user_email):
     fromaddr = 'savethewarrior@gmail.com'
     toUser_email = user_email
+
     msg = "\r\n".join([
         "From: savethewarrior@gmail.com",
         "To:" + toUser_email + "@gmail.com",
@@ -34,8 +58,8 @@ def sendEmail(user_email, toUser_birthday):
 
 
 def job(user_birthday, user_email): #Function called when date of birth is today
-    print("It's", user_birthday,"\b's Birthday!\n")
-    sendEmail(user_email, user_birthday)
+    print("It's", user_birthday,"\b's Birthday!")
+    sendEmailGrid(user_birthday, user_email)
 
 try:
     #Connection to database
@@ -53,7 +77,7 @@ try:
     user_records = cursor.fetchall()
     for row in user_records:
         print("DOB = ", row[0], )
-        print("username = ", row[1], "\n")
+        print("username = ", row[1], )
         print("email = ", row[2], "\n")
         print("Birthday(month-date)", datetime.strptime(str(row[0]), date_format).date().month, "-", datetime.strptime(str(row[0]), date_format).date().day)
         print("Today(month-date)",datetime.now().date().month, "-", datetime.now().date().day,"\n")
